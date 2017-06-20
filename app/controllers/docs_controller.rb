ef1d1e10039615fe35c6751043a0ddfa61efb1cb
Keshava Kotri <keshav143420@gmail.com>
2017-06-20 22:43:54 +0530

    Doc controller and routes

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,36 @@
+class DocsController < ApplicationController
+
+	def index
+	end
+
+	def create
+	end
+
+	def new
+	end
+
+	def edit
+	end
+
+	def show
+	end
+
+	def update
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+		def find_doc
+		end
+
+		def doc_params
+		end
+
+
+end
